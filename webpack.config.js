3f137d4a13dd43df3971ff5c3ab218191a479eb8
evenushka <evenushkauw@mail.ru>
2023-09-29 19:17:16 +0300

    Test path method.

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,10 @@
-// mode - режим сборки, entry - точка входа
+// Модуль path, метод resolve. Принимает относительный путь и делает из него абсолютный от корня операционной системы.
+// Превращает 'dist' в C:\Users\Eve\Desktop\workspace\webpacktest\dist.
+import path from 'path';
+let test = path.resolve('dist');
+console.log(test);
+
+// mode - режим сборки, entry - точка входа.
 export default {
     mode: 'development',
     entry: './src/index.js',
