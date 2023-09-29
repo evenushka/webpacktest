ef1f0cfbe7374afe605050bc557d924f74d1c331
evenushka <evenushkauw@mail.ru>
2023-09-29 21:44:02 +0300

    Add html support.

--- src/index.js
+++ src/index.js
@@ -1,2 +1,5 @@
+import './index.html';
+
 const mult = (a, b) => a*b;
 console.log(mult(4, 5));
+console.log(mult(4, 123));
---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const path = require('path');
+
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const mode = process.env.NODE_ENV || 'development';
+const devMode = mode === 'development';
+
+module.exports = {
+    mode,
+    entry: path.resolve(__dirname, 'src', 'index.js'),
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        clean: true,
+        filename: "[name].[contenthash].js"
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: path.resolve(__dirname, 'src', 'index.html'),
+        }),
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.html$/i,
+                loader: 'html-loader',
+            }
+        ]
+    }
+}
